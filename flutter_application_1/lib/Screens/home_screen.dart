508c027a8647dd47014d3642e65cf8830822a763
OverCome321 <fanelog49@gmail.com>
2022-10-22 22:27:04 +0300

    Дополнил окна

--- flutter_application_1/lib/Screens/home_screen.dart
+++ flutter_application_1/lib/Screens/home_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'package:flutter_application_1/Screens/ideas_screen';
+import 'package:flutter_application_1/Screens/ideas_screen.dart';
 
 import 'package:flutter_application_1/Screens/profile.dart';
 
@@ -50,108 +50,7 @@ class _HomeScreenState extends State<HomeScreen> {
           )
         ],
       ),
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        title: Text(
-          "Search",
-          style: TextStyle(
-            color: Color.fromARGB(255, 77, 77, 77),
-          ),
-        ),
-        actions: [
-          IconButton(
-            color: Color.fromARGB(255, 77, 77, 77),
-            onPressed: () {
-              showSearch(
-                context: context,
-                delegate: CustomSearchDelegate(),
-              );
-            },
-            icon: const Icon(Icons.search),
-          ),
-        ],
-      ),
       body: screens[_currentIndex],
     );
-  }
-}
-
-class CustomSearchDelegate extends SearchDelegate {
-  List<String> searchTerms = [
-    'Unreal Engine',
-    'C#',
-    'Flutter',
-    'Java',
-    'JavaScript',
-    'Kotlin',
-    'Swift',
-    'Unity',
-    'Python',
-    'C++',
-  ];
-  @override
-  List<Widget> buildActions(BuildContext context) {
-    return [
-      IconButton(
-        onPressed: () {
-          query = '';
-        },
-        icon: const Icon(Icons.clear),
-      ),
-    ];
-  }
-
-  @override
-  Widget buildLeading(BuildContext context) {
-    return IconButton(
-      onPressed: () {
-        close(context, null);
-      },
-      icon: const Icon(Icons.arrow_back),
-    );
-  }
-
-  @override
-  Widget buildResult(BuildContext context) {
-    List<String> matchQuery = [];
-    for (var language in searchTerms) {
-      if (language.toLowerCase().contains(query.toLowerCase())) {
-        matchQuery.add(language);
-      }
-    }
-    return ListView.builder(
-      itemCount: matchQuery.length,
-      itemBuilder: (context, index) {
-        var result = matchQuery[index];
-        return ListTile(
-          title: Text(result),
-        );
-      },
-    );
-  }
-
-  @override
-  Widget buildSuggestions(BuildContext context) {
-    List<String> matchQuery = [];
-    for (var language in searchTerms) {
-      if (language.toLowerCase().contains(query.toLowerCase())) {
-        matchQuery.add(language);
-      }
-    }
-    return ListView.builder(
-      itemCount: matchQuery.length,
-      itemBuilder: (context, index) {
-        var result = matchQuery[index];
-        return ListTile(
-          title: Text(result),
-        );
-      },
-    );
-  }
-
-  @override
-  Widget buildResults(BuildContext context) {
-    // TODO: implement buildResults
-    throw UnimplementedError();
   }
 }
