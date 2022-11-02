6d4ca069e647d299f88eb19e21a50614bab9025d
OverCome321 <fanelog49@gmail.com>
2022-11-02 17:59:37 +0300

    Changed profile screen

--- flutter_application_1/lib/Screens/profile.dart
+++ flutter_application_1/lib/Screens/profile.dart
@@ -23,79 +23,112 @@ class _ProfileScreenCreateState extends State<ProfileScreen> {
       body: SingleChildScrollView(
         child: Column(
           children: <Widget>[
-            SizedBox(
-              height: 25,
+            Padding(
+              padding:
+                  EdgeInsets.only(top: 30, left: 15, right: 15, bottom: 20),
+              child: Container(
+                height: 300,
+                width: 380,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  color: Color.fromARGB(255, 230, 230, 230),
+                ),
+                child: Column(
+                  children: <Widget>[
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Align(
+                      alignment: Alignment.topCenter,
+                      child: CircleAvatar(
+                        backgroundColor: Colors.white,
+                        radius: 90,
+                        child: Text(
+                          'NG',
+                          style: TextStyle(
+                              fontSize: 25,
+                              color: Color.fromARGB(255, 0, 0, 0)),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      'Nikita Coolest',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.black,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 14,
+                    ),
+                    Text(
+                      'SearchAt user',
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
             Container(
-              height: 300,
-              width: 400,
+              height: 235,
+              width: 380,
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(30),
                 color: Color.fromARGB(255, 230, 230, 230),
               ),
               child: Column(
-                children: <Widget>[
-                  SizedBox(
-                    height: 20,
-                  ),
+                children: [
                   Align(
-                    alignment: Alignment.topCenter,
-                    child: CircleAvatar(
-                      backgroundColor: Colors.white,
-                      radius: 90,
+                    alignment: Alignment.topLeft,
+                    child: Padding(
+                      padding: EdgeInsets.only(left: 20, top: 20, bottom: 10),
                       child: Text(
-                        'NG',
-                        style: TextStyle(fontSize: 25, color: Colors.white),
+                        'About yourself:',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.black,
+                        ),
                       ),
                     ),
                   ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Text(
-                    'Nikita Coolest',
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.w500,
-                      color: Colors.black,
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 20),
+                    child: Divider(
+                      color: Color.fromARGB(255, 77, 77, 77),
                     ),
                   ),
-                  SizedBox(
-                    height: 14,
-                  ),
-                  Text(
-                    'SearchAt user',
-                    style: TextStyle(
-                      color: Colors.grey,
-                      fontSize: 16,
+                  Padding(
+                    padding: EdgeInsets.only(left: 10, right: 10, top: 2),
+                    child: TextFormField(
+                      maxLength: 300,
+                      minLines: 3,
+                      maxLines: 5,
+                      decoration: InputDecoration(
+                        hintText: 'Type Anything about yourself',
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15.0),
+                          borderSide: BorderSide(
+                            color: Color.fromARGB(255, 230, 230, 230),
+                          ),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Color.fromARGB(255, 230, 230, 230),
+                          ),
+                          borderRadius: BorderRadius.circular(15.0),
+                        ),
+                      ),
                     ),
                   ),
                 ],
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Container(
-              height: 200,
-              width: 400,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(30),
-                color: Color.fromARGB(255, 230, 230, 230),
-              ),
-              child: Align(
-                alignment: Alignment.bottomLeft,
-                child: Padding(
-                  padding: EdgeInsets.only(left: 20),
-                  child: Text(
-                    'About',
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.w500,
-                      color: Colors.black,
-                    ),
-                  ),
-                ),
               ),
             ),
           ],
